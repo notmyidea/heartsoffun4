4cfec6f2643cc424e902c65496f281aaf7399dcc
notswedish <107358985+notswedish@users.noreply.github.com>
2024-10-12 23:48:57 +0200

    Initial commit

---
+++ common/defines/01_defines_divisionlimit.lua
@@ -0,0 +1,5 @@
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 60			
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1				
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0			
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 0		
+-- from # "More Division Limits - 60 division cap version"
---
+++ descriptor.mod
@@ -0,0 +1,12 @@
+version="1.0r1"
+tags={
+	"Gameplay"
+	"Historical"
+	"Balance"
+	"Fixes"
+	"Map"
+	"Utilities"
+}
+name="Hearts of Fun 4"
+supported_version="1.14.9"
+remote_file_id="3347688724"
